69837f811800b03f427e10c1ef25a4d52ccb3508
Milan <mp09092003@gmail.com>
2025-09-12 22:58:12 +0200

    member naar properties en contructor waardes gekoppeld

--- Grocery.Core/Models/GroceryListItem.cs
+++ Grocery.Core/Models/GroceryListItem.cs
@@ -2,12 +2,43 @@
 {
     public class GroceryListItem : Model
     {
-        public int GroceryListId; //To do: change it to a property
-        public int ProductId;     //To do: change it to a property
-        public int Amount { get; set; }
+        public int GroceryListId    //Changed to a property
+        {
+            get { return GroceryListId; }
+
+            set { GroceryListItem = value; }
+        } 
+
+        public int ProductId    //Changed to a property 
+        {
+            get { return ProductId; }
+
+            set { ProductId = value; }
+        }
+
+        public int Amount   //Property format changed
+        {
+            get { return Amount; }
+
+            set { Amount = value; }
+        }
+
+        public int Id   //Property added
+        {
+            get { return Id; }
+
+            set { Id = value; }
+        }
+
         public GroceryListItem(int id, int groceryListId, int productId, int amount) : base(id, "")
         {
-            //To do:assign values to the properties
+            //Values assigned to the properties
+
+            Id = id;
+            GroceryListId = groceryListId;
+            ProductId = productId;
+            Amount = amount;
+
         }
 
         public Product Product { get; set; } = new(0, "None", 0);
