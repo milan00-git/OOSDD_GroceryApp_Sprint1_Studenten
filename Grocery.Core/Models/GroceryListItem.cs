b7b63fae37adc2171885c29fa8b360effca79ce0
Milan <mp09092003@gmail.com>
2025-09-12 23:35:15 +0200

    properties gecorrigeerd

--- Grocery.Core/Models/GroceryListItem.cs
+++ Grocery.Core/Models/GroceryListItem.cs
@@ -2,33 +2,11 @@
 {
     public class GroceryListItem : Model
     {
-        public int GroceryListId    //Changed to a property
-        {
-            get { return GroceryListId; }
+        public int GroceryListId { get; set; }  //Changed to a (auto) property 
 
-            set { GroceryListItem = value; }
-        } 
+        public int ProductId { get; set; }   //Changed to a (auto) property 
 
-        public int ProductId    //Changed to a property 
-        {
-            get { return ProductId; }
-
-            set { ProductId = value; }
-        }
-
-        public int Amount   //Property format changed
-        {
-            get { return Amount; }
-
-            set { Amount = value; }
-        }
-
-        public int Id   //Property added
-        {
-            get { return Id; }
-
-            set { Id = value; }
-        }
+        public int Amount { get; set; }
 
         public GroceryListItem(int id, int groceryListId, int productId, int amount) : base(id, "")
         {
